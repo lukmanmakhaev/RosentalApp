2a580746b501873553bd8b6383778c1b0b778549
Lukman <lukmanmakhaev@gmail.com>
2024-09-03 19:55:33 +0300

    Initial Commit

---
+++ RosentalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RosentalApp
+//
+//  Created by Lukman Makhaev on 03.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
